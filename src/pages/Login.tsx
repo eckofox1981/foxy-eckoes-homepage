130b20ffc6379375c33e7347e3eaf8e66fd5659b
eckofox1981 <eckofox1981@pm.me>
2025-11-02 19:01:42 +0100

    feat: add LoginPage with login form, register form, relevant css and register check functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,214 @@
+import "../styles/login.css";
+import "../styles/buttons.css";
+import foxyEckoesLogo from "../assets/logos/F-E-logo-title.png";
+import { useState } from "react";
+import { User } from "../models/User";
+import classNames from "classnames";
+
 export function LoginPage() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [usernameRegister, setUsernameRegister] = useState<string>("");
+  const [passwordRegister, setPasswordRegister] = useState<string>("");
+  const [confirm, setConfirm] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string[]>([]);
+
+  const handleRegister = () => {
+    const user: User = new User(
+      "",
+      usernameRegister,
+      firstName,
+      lastName,
+      email,
+      [],
+      "user",
+      ""
+    );
+    const ifMessage: string[] = checkRegisterData(
+      user,
+      passwordRegister,
+      confirm
+    );
+    setMessage(ifMessage);
+    if (ifMessage.length === 0) {
+      console.log("You'd register");
+    }
+  };
+
+  const Message = () => {
+    if (message.length > 0) {
+      return (
+        <div className="error-messages">
+          {message.map((message, index) => (
+            <p key={index}>{message}</p>
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
-    <main>
-      <section>
-        <form action="login">
-          <input type="text" />
-          <input type="password" />
+    <main className="login-main">
+      <img src={foxyEckoesLogo} alt="Foxy Eckoes logo" />
+      <section className={classNames("login-section", "withBorderBottom")}>
+        <h2>Login to book one of our amazing events!</h2>
+        <form action="login" className="login-form">
+          <div className="form-field">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              placeholder="username"
+              onChange={() => setUsername(event?.target?.value || "")}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              onChange={() => setPassword(event?.target?.value || "")}
+            />
+          </div>
         </form>
+        <button className="menu-button">Login</button>
+      </section>
+      <section className="login-section">
+        <h3>
+          You don't have account? Enroll and book yourselves to some amazing
+          entertainement!
+        </h3>
+        <form action="register">
+          <div className="form-field">
+            <label htmlFor="usernameRegister">Username</label>
+            <input
+              id="usernameRegister"
+              type="text"
+              placeholder="username"
+              onChange={() => setUsernameRegister(event?.target?.value || "")}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="passwordRegister">Password</label>
+            <input
+              id="passwordRegister"
+              type="password"
+              placeholder="password"
+              onChange={() => {
+                setPasswordRegister(event?.target?.value || "");
+              }}
+            />
+          </div>{" "}
+          <div className="form-field">
+            <label htmlFor="password-confirmRegister">Confirm</label>
+            <input
+              id="password-confirmRegister"
+              type="password"
+              placeholder="confirm password"
+              onChange={() => {
+                setConfirm(event?.target?.value || "");
+              }}
+            />
+          </div>{" "}
+          <div className="form-field">
+            <label htmlFor="firstname">First Name</label>
+            <input
+              id="firstname"
+              type="text"
+              placeholder="John"
+              onChange={() => {
+                setFirstName(event?.target?.value || "");
+              }}
+            />
+          </div>{" "}
+          <div className="form-field">
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              id="lastname"
+              type="text"
+              placeholder="Doe"
+              onChange={() => {
+                setLastName(event?.target?.value || "");
+              }}
+            />
+          </div>{" "}
+          <div className="form-field">
+            <label htmlFor="email">E-mail</label>
+            <input
+              id="email"
+              type="text"
+              placeholder="john.doe@example.com"
+              onChange={() => setEmail(event?.target?.value || "")}
+            />
+          </div>
+        </form>
+        <button className="menu-button" onClick={handleRegister}>
+          Register
+        </button>
+      </section>
+      <section className="login-message">
+        <Message />
       </section>
     </main>
   );
+}
+
+function checkRegisterData(user: User, password: string, confirm: string) {
+  let errors: string[] = [];
+
+  if (!user.username) {
+    errors.push("Please enter a username.");
+  }
+  if (!password) {
+    errors.push("Please enter a password.");
+  } else {
+    if (password.length <= 5) {
+      errors.push(
+        "Password must be at least 6 characters long, please try again."
+      );
+    }
+    if (!/(?=.*[a-z])/.test(password)) {
+      errors.push(
+        "Password must contain at least one lowercase letter, please try again."
+      );
+    }
+    if (!/(?=.*[A-Z])/.test(password)) {
+      errors.push(
+        "Password must contain at least one uppercase letter, please try again."
+      );
+    }
+    if (!/(?=.*\d)/.test(password)) {
+      errors.push(
+        "Password must contain at least one number, please try again."
+      );
+    }
+    if (!/^[a-zA-Z0-9]+$/.test(password)) {
+      errors.push(
+        "Password can only contain letters and numbers, please try again."
+      );
+    }
+  }
+  if (!password) {
+    errors.push("Please enter a password.");
+  }
+  if (password !== confirm) {
+    errors.push("Your passwords do not match, please try again.");
+  }
+  if (!user.firstName) {
+    errors.push("Please enter a first name.");
+  }
+  if (!user.lastName) {
+    errors.push("Please enter a last name.");
+  }
+  if (!user.email) {
+    errors.push("Please enter an email.");
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
+    errors.push("Please enter a valid email address.");
+  }
+
+  return errors;
 }
