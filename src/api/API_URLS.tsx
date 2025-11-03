a57aa7a645cf08afaf60378da65c3d91de8600d2
eckofox1981 <eckofox1981@pm.me>
2025-11-03 13:49:13 +0100

    fix: error in GET_EVENT_BY_ID_URL => add missing '='

--- src/api/API_URLS.tsx
+++ src/api/API_URLS.tsx
@@ -1,4 +1,4 @@
 export const GET_ALL_EVENT_URL: string = "http://localhost:8080/event/get-all";
 
 export const GET_EVENT_BY_ID_URL: string =
-  "http://localhost:8080/event/id?eventID";
+  "http://localhost:8080/event/id?eventID=";
