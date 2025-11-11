47331db5b6d728ec3174bed2f78c82a0f0a58d52
eckofox1981 <eckofox1981@pm.me>
2025-11-11 18:01:30 +0100

    fix: modify all api call to call relative to backend URL when set up with containers

--- src/api/API_URLS.tsx
+++ src/api/API_URLS.tsx
@@ -1,40 +1,35 @@
-export const GET_ALL_EVENT_URL: string = "http://localhost:8080/event/get-all";
+export const GET_ALL_EVENT_URL: string = "/event/get-all";
 
-export const GET_EVENT_BY_ID_URL: string =
-  "http://localhost:8080/event/id?eventID=";
+export const GET_EVENT_BY_ID_URL: string = "/event/id?eventID=";
 
-export const CREATE_EVENT_URL_ADMIN = "http://localhost:8080/event/create";
+export const CREATE_EVENT_URL_ADMIN = "/event/create";
 
-export const DELETE_EVENT_URL_ADMIN = "http://localhost:8080/event/cancel";
+export const DELETE_EVENT_URL_ADMIN = "/event/cancel";
 
-export const FILTER_EVENT_URL = "http://localhost:8080/event/filter";
+export const FILTER_EVENT_URL = "/event/filter";
 
-export const UPDATE_EVENT_URL_ADMIN = "http://localhost:8080/event/update";
+export const UPDATE_EVENT_URL_ADMIN = "/event/update";
 
 export const CONTROL_ALL_EVENT_SEAT_AVAILIBILITY_URL_ADMIN =
-  "http://localhost:8080/event/control-all-event-availability";
+  "/event/control-all-event-availability";
 
-export const LOGIN_URL: string = "http://localhost:8080/user/login";
+export const LOGIN_URL: string = "/user/login";
 
-export const GET_USER_INFO_URL: string = "http://localhost:8080/user/info";
+export const GET_USER_INFO_URL: string = "/user/info";
 
-export const REGISTER_USER_URL: string = "http://localhost:8080/user/create";
+export const REGISTER_USER_URL: string = "/user/create";
 
-export const UPDATE_USER_URL: string = "http://localhost:8080/user/update";
+export const UPDATE_USER_URL: string = "/user/update";
 
-export const GET_ALL_USERS_URL_ADMIN =
-  "http://localhost:8080/user/get-all-usernames-and-id";
+export const GET_ALL_USERS_URL_ADMIN = "/user/get-all-usernames-and-id";
 
-export const BOOK_EVENT_URL: string = "http://localhost:8080/booking/book";
+export const BOOK_EVENT_URL: string = "/booking/book";
 
-export const CANCEL_BOOKING_URL: string =
-  "http://localhost:8080/booking/cancel";
+export const CANCEL_BOOKING_URL: string = "/booking/cancel";
 
-export const UPDATE_BOOKING_URL: string =
-  "http://localhost:8080/booking/update";
+export const UPDATE_BOOKING_URL: string = "/booking/update";
 
 export const GET_ALL_BOOKINGS_BY_USERID_URL_ADMIN =
-  "http://localhost:8080/booking/get-bookings-by-userId";
+  "/booking/get-bookings-by-userId";
 
-export const DELETE_BOOKING_BY_BOOKINGID_ADMIN =
-  "http://localhost:8080/booking/delete";
+export const DELETE_BOOKING_BY_BOOKINGID_ADMIN = "/booking/delete";
